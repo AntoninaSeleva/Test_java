8cba949e53cc663654add55e8f9af4bd99a3e64d
AntoninaSeleva <aseleva@usetech.ru>
2019-11-14 13:40:19 +0300

    Добавлены функции

--- sandbox/src/main/java/hello/JavaFirst.java
+++ sandbox/src/main/java/hello/JavaFirst.java
@@ -3,6 +3,24 @@ package hello;
 public class JavaFirst {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        hello("world");
+        hello("user");
+        double len =5;
+        System.out.println("Area = " + area(len));
+        double a=4;
+        double b= 5;
+        System.out.println("Area2 = " + area(a,b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("hello, " + somebody);
+    }
+
+    public static double area(double l){
+        return l *l;
+    }
+
+    public  static double area (double a, double b){
+        return a*b;
     }
 }
