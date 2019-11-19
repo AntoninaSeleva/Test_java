40a7606a6ccaa0a8f66f3d322cac54155e1c011c
AntoninaSeleva <aseleva@usetech.ru>
2019-11-19 18:58:16 +0300

    Работающий тест по видео 2.2

--- addressbook_web/src/test/java/qa/addressbook/TestAddressTest.java
+++ addressbook_web/src/test/java/qa/addressbook/TestAddressTest.java
@@ -3,6 +3,8 @@ package qa.addressbook;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,12 +13,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.HashMap;
 import java.util.Map;
 public class TestAddressTest {
-    //private WebDriver driver;
-    //private Map<String, Object> vars;
-    //JavascriptExecutor js;
-   /* @Before
+    private WebDriver driver;
+    private Map<String, Object> vars;
+    JavascriptExecutor js;
+    @Before
     public void setUp() {
-        //System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\aseleva\\Documents\\GitHub\\Test_java\\addressbook_web\\src\\test\\java\\qa\\addressbook\\chromedriver.exe");
         driver = new ChromeDriver();
         js = (JavascriptExecutor) driver;
         vars = new HashMap<String, Object>();
@@ -24,11 +26,15 @@ public class TestAddressTest {
     @After
     public void tearDown() {
         driver.quit();
-    }*/
+    }
     @Test
     public void testAddress() {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\aseleva\\Documents\\GitHub\\Test_java\\addressbook_web\\src\\test\\java\\qa\\addressbook\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://www.google.com");
+        driver.get("http://localhost/addressbook/");
+        //driver.findElement(By.linkText("Logout")).click();
+        driver.findElement(By.name("user")).click();
+        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.cssSelector("input:nth-child(7)")).click();
     }
 }
