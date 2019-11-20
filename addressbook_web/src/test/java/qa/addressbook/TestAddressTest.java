c7ae043e6978b17ae990599c08256326f368856d
AntoninaSeleva <aseleva@usetech.ru>
2019-11-20 14:29:26 +0300

    Рефакторинг. Вынесение функций теста

--- addressbook_web/src/test/java/qa/addressbook/TestAddressTest.java
+++ addressbook_web/src/test/java/qa/addressbook/TestAddressTest.java
@@ -11,39 +11,66 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.HashMap;
 import java.util.Map;
+
 public class TestAddressTest {
     private WebDriver driver;
     private Map<String, Object> vars;
     JavascriptExecutor js;
+
     @Before
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\aseleva\\Documents\\GitHub\\Test_java\\addressbook_web\\src\\test\\java\\qa\\addressbook\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\aseleva\\Documents\\GitHub\\Test_java\\addressbook_web\\src\\test\\java\\qa\\addressbook\\chromedriver.exe");
         driver = new ChromeDriver();
         js = (JavascriptExecutor) driver;
         vars = new HashMap<String, Object>();
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
         driver.get("http://localhost/addressbook/");
         driver.findElement(By.name("user")).click();
-        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("user")).sendKeys(username);
         driver.findElement(By.name("pass")).click();
-        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.name("pass")).sendKeys(password);
         driver.findElement(By.cssSelector("input:nth-child(7)")).click();
     }
+
     @After
     public void tearDown() {
         driver.quit();
     }
+
     @Test
     public void testAddress() {
-        driver.get("http://localhost/addressbook/group.php");
-        driver.findElement(By.linkText("groups")).click();
-        driver.findElement(By.name("new")).click();
-        driver.findElement(By.name("group_name")).click();
-        driver.findElement(By.name("group_name")).sendKeys("Test1");
-        driver.findElement(By.name("group_header")).click();
-        driver.findElement(By.name("group_header")).sendKeys("Test2");
-        driver.findElement(By.name("group_footer")).click();
-        driver.findElement(By.name("group_footer")).sendKeys("Test3");
-        driver.findElement(By.name("submit")).click();
+        goToGroup();
+        initGroupCreation();
+        fillGroupForm(new GroupData("Test1", "Test2", "Test3"));
+        submitGroupCreation();
+        returnToGroupPage();
+    }
+
+    private void returnToGroupPage() {
         driver.findElement(By.linkText("group page")).click();
+    }
+
+    private void submitGroupCreation() {
+        driver.findElement(By.name("submit")).click();
+    }
+
+    private void fillGroupForm(GroupData groupData) {
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).sendKeys(groupData.getName());
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    private void initGroupCreation() {
+        driver.findElement(By.name("new")).click();
+    }
+
+    private void goToGroup() {
+        driver.findElement(By.linkText("groups")).click();
     }
 }
