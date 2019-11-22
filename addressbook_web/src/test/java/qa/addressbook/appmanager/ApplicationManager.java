d1bf7d28bac0b20db398a50501af1cc8fa56973f
AntoninaSeleva <aseleva@usetech.ru>
2019-11-22 17:05:54 +0300

    Использование нескольких браузеров

--- addressbook_web/src/test/java/qa/addressbook/appmanager/ApplicationManager.java
+++ addressbook_web/src/test/java/qa/addressbook/appmanager/ApplicationManager.java
@@ -3,6 +3,9 @@ package qa.addressbook.appmanager;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,15 +16,27 @@ public class ApplicationManager {
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
+    private String browser;
 
     JavascriptExecutor js;
     private Map<String, Object> vars;
 
+    public ApplicationManager(String browser) {
+        this.browser=browser;
+    }
+
     public void init() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\aseleva\\Documents\\GitHub\\Test_java\\addressbook_web\\src\\test\\java\\qa\\addressbook\\chromedriver.exe");
-        driver = new ChromeDriver();
+        if(browser.equals(BrowserType.CHROME)){
+            driver = new ChromeDriver();
+        } else if(browser.equals(BrowserType.FIREFOX)){
+            driver = new FirefoxDriver();
+        }else if(browser.equals(BrowserType.IE)){
+            driver = new InternetExplorerDriver();
+        }
+
         js = (JavascriptExecutor) driver;
         vars = new HashMap<String, Object>();
+        driver.get("http://localhost/addressbook");
         groupHelper = new GroupHelper(driver);
         navigationHelper = new NavigationHelper(driver);
         sessionHelper = new SessionHelper(driver);
--- addressbook_web/src/test/java/qa/addressbook/tests/TestBase.java
+++ addressbook_web/src/test/java/qa/addressbook/tests/TestBase.java
@@ -2,11 +2,12 @@ package qa.addressbook.tests;
 
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.remote.BrowserType;
 import qa.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @Before
     public void setUp() {
